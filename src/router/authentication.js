a0350ed754f6d190c7adc357152425dfc016f36c
José Lugo <chepe.time@gmail.com>
2023-08-29 18:22:59 -0600

    Improved README

--- src/router/authentication.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var authentication_1 = require("./../controllers/authentication");
-exports.default = (function (router) {
-    router.post("/auth/register", authentication_1.register);
-    router.post("/auth/login", authentication_1.login);
-});
--- src/router/users.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var users_1 = require("../controllers/users");
-var middlewares_1 = require("../middlewares");
-exports.default = (function (router) {
-    router.get("/users", middlewares_1.isAuthenticated, users_1.getAllUsers);
-    router.delete("/users/:id", middlewares_1.isAuthenticated, middlewares_1.isOwner, users_1.deleteUser);
-    router.patch("/users/:id", middlewares_1.isAuthenticated, middlewares_1.isOwner, users_1.updateUser);
-});
